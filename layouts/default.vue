dcfa058af4937077bad050a54542a5f98e34fd13
gnehs <jayuiop@gmail.com>
2019-12-20 21:16:17 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<v-app-bar app flat dark>
+			<v-toolbar-title style="padding-left: 32px">
+				<span>台灣數位串流有限公司</span>
+			</v-toolbar-title>
+			<v-spacer />
+			<v-toolbar-items>
+				<v-btn text>首頁</v-btn>
+				<v-btn text>簡介</v-btn>
+				<v-btn text>理念</v-btn>
+				<v-btn text>營運項目</v-btn>
+				<v-btn text>自主開發商品</v-btn>
+			</v-toolbar-items>
+		</v-app-bar>
+		<v-content>
+			<router-view />
+		</v-content>
+		<v-snackbar v-model="snackbar.show">{{snackbar.message}}</v-snackbar>
+	</div>
+</template>
+<script>
+import Vue from "vue";
+export default {
+	name: "App",
+	data: () => ({
+		drawer: null,
+		snackbar: {
+			show: false,
+			message: ``,
+			timeout: null
+		},
+	}),
+	destroyed() {
+	},
+	created() {
+		//註冊點心條組件
+		Vue.prototype.$snackbar = (msg = ``, duration = 1500) => {
+			this.snackbar.message = msg;
+			this.snackbar.show = true;
+			clearTimeout(this.snackbar.timeout);
+			this.snackbar.timeout = setTimeout(
+				() => (this.snackbar.show = false),
+				duration
+			);
+		};
+
+	},
+	methods: {
+
+	}
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,87 @@
+<template>
+	<div>
+		<div id="header">
+			<div class="container">
+				<p class="header__title">台灣數位串流有限公司</p>
+				<p class="header__subtitle">Taiwan Digital Streaming Co.</p>
+			</div>
+		</div>
+		<div class="container landing__container">
+			<section>
+				<div class="landing__title">簡介</div>
+				<p>發源自開源社群、擁抱，並持續貢獻於開源社群，在注重開源的同時不忘資安上的保護。</p>
+			</section>
+			<section>
+				<div class="landing__title">理念</div>
+				<p>發大財。</p>
+			</section>
+			<section>
+				<div class="landing__title">營運項目</div>
+				<v-row class="mb-6" no-gutters>
+					<v-col md="4" sm="2">
+						<div class="service__item">
+							<div class="service__item__icon">
+								<v-icon class="outline-gavel"></v-icon>
+							</div>
+							<div class="service__item__title">網路設備批發/零售</div>
+						</div>
+					</v-col>
+					<v-col md="4" sm="2">
+						<div class="service__item">
+							<div class="service__item__icon">
+								<v-icon class="outline-devices"></v-icon>
+							</div>
+							<div class="service__item__title">網頁應用程式開發與維護</div>
+						</div>
+					</v-col>
+					<v-col md="4" sm="2">
+						<div class="service__item">
+							<div class="service__item__icon">
+								<v-icon class="outline-adb"></v-icon>
+							</div>
+							<div class="service__item__title">系統與設備維護</div>
+						</div>
+					</v-col>
+					<v-col md="4" sm="2">
+						<div class="service__item">
+							<div class="service__item__icon">
+								<v-icon class="outline-check"></v-icon>
+							</div>
+							<div class="service__item__title">應用程式解決方案</div>
+						</div>
+					</v-col>
+					<v-col md="4" sm="2">
+						<div class="service__item">
+							<div class="service__item__icon">
+								<v-icon class="outline-storage"></v-icon>
+							</div>
+							<div class="service__item__title">網站代管營運</div>
+						</div>
+					</v-col>
+					<v-col md="4" sm="2">
+						<div class="service__item">
+							<div class="service__item__icon">
+								<v-icon class="outline-widgets"></v-icon>
+							</div>
+							<div class="service__item__title">容器化解決方案</div>
+						</div>
+					</v-col>
+				</v-row>
+			</section>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	data: () => ({
+	}),
+	destroyed() {
+	},
+	created() {
+
+	},
+	methods: {
+
+	}
+};
+</script>
