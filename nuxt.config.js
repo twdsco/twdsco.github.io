659576cb478d67bba5d56ec30ff99c0937910d9b
gnehs <jayuiop@gmail.com>
2019-12-21 00:19:25 +0800

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s - 台灣數位串流有限公司',
+    title: '台灣數位串流有限公司',
     meta: [{
         charset: 'utf-8'
       },
