a2bada636eed17379f8a744bf52154a65fdd8241
gnehs <gnehs@gnehs.net>
2024-04-04 18:06:37 +0800

    updated website

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,109 @@
-import Image from "next/image";
+import {
+  Store,
+  AppWindowMac,
+  CloudCog,
+  Smartphone,
+  Server,
+  Container,
+} from "lucide-react";
+interface ServiceItem {
+  icon: JSX.Element;
+  title: string;
+}
+
+interface NakamaItem {
+  img: string;
+  url: string;
+}
 
 export default function Home() {
+  const serviceItems: ServiceItem[] = [
+    { icon: <Store size={48} />, title: "網路設備批發/零售" },
+    { icon: <AppWindowMac size={48} />, title: "網頁應用程式開發與維護" },
+    { icon: <CloudCog size={48} />, title: "系統與設備維護" },
+    { icon: <Smartphone size={48} />, title: "應用程式解決方案" },
+    { icon: <Server size={48} />, title: "網站代管營運" },
+    { icon: <Container size={48} />, title: "容器化解決方案" },
+  ];
+
+  const nakama: NakamaItem[] = [
+    { img: "proxmox.png", url: "https://www.proxmox.com" },
+    { img: "unitech.svg", url: "https://www.unitech.com.tw/" },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div
+        id="banner"
+        className="bg-cover bg-center text-white h-1/2 min-h-[410px] flex justify-center items-center text-shadow-md font-bold bg-[url('/img/background.jpg')]"
+      >
+        <div className="container mx-auto">
+          <img
+            src="/logo/twds_white.svg"
+            alt="Taiwan Digital Streaming Co. Logo"
+            className="banner__logo"
+            width="128"
+          />
+          <p className="banner__title text-6xl">台灣數位串流有限公司</p>
+          <p className="banner__subtitle text-4xl">
+            Taiwan Digital Streaming Co.
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="container mx-auto text-center my-8 flex flex-col gap-8">
+        {/* 簡介部分 */}
+        <section>
+          <div className="landing__title text-3xl font-bold underline decoration-blue-600 decoration-4 mb-2 inline-block">
+            簡介
+          </div>
+          <p>
+            發源自開源社群、擁抱，並持續貢獻於開源社群，在注重開源的同時不忘資安上的保護。
+          </p>
+        </section>
+        {/* 營運項目 */}
+        <section>
+          <div className="text-3xl font-bold underline decoration-blue-600 decoration-4 mb-2 inline-block">
+            營運項目
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {serviceItems.map((item, index) => (
+              <div
+                key={index}
+                className=" bg-gradient-to-br from-blue-500 to-blue-700 p-4 rounded-lg shadow-md"
+              >
+                {/* 替換成Font Awesome或相似圖示系統 */}
+                <div className="flex items-center justify-center text-white mb-2">
+                  {item.icon}
+                </div>
+                <div className="text-white font-bold">{item.title}</div>
+              </div>
+            ))}
+          </div>
+        </section>
+        {/* 經銷/合作 */}
+        <section>
+          <div className=" text-3xl font-bold underline decoration-blue-600 decoration-4 mb-2 inline-block">
+            經銷/合作
+          </div>
+          <div className=" flex justify-center items-center">
+            {nakama.map((item, index) => (
+              <a
+                key={index}
+                href={item.url}
+                target="_blank"
+                className="inline-block m-2"
+                style={{
+                  backgroundImage: `url('/img/partners/${item.img}')`,
+                  width: "300px",
+                  height: "64px",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "contain",
+                  backgroundPosition: "center",
+                }}
+              ></a>
+            ))}
+          </div>
+        </section>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
