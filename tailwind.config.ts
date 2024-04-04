60b577df2db8b3a71d6a7cb8d5f06b5c77192a13
gnehs <gnehs@gnehs.net>
2024-04-04 18:21:25 +0800

    Update tailwind.config.ts with container padding and screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,17 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "2rem",
+      },
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
   },
